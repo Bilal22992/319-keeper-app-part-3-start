1bbeaac17ad84e320320a0f9780f7e5171a1d3e5
Bilal22992 <haiderbilal22992@gmail.com>
2025-11-24 20:06:14 +0500

    added backend for add and read

--- 319-keeper-app-part-3-starting/src/components/App.jsx
+++ 319-keeper-app-part-3-starting/src/components/App.jsx
@@ -1,13 +1,52 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+
 function App() {
   const [notes, setnotes] = useState([]);
+  const[data,setdata]=useState([]);
+
+
+useEffect(() =>
+{
+
+  const fetchNotes =() => {
+
+  fetch("http://localhost:5000").then(
+    response =>response.json()
+
+  ).then((comingdata)=> setnotes(comingdata))
+  }
+  
+  fetchNotes();
+
+
+  const Refresh= setInterval(fetchNotes,2000);
+
+  return () => clearInterval(Refresh);
+    
+  
+}, [])
+ 
+
 
   function addItem(title, note) {
+const Newdata = {title,note}
+    fetch("http://localhost:5000",{
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json", 
+      },
+      body: JSON.stringify(Newdata), 
+    }).then(res=>res.json()).then(response =>{
+      console.log("server response",response)
+    })
+
+
+
     setnotes((prev) => {
       return [
         ...prev,
@@ -30,6 +69,10 @@ function App() {
   return (
     <div>
       <Header />
+      {data.map((data,index)=>{
+       return <p key={index}>{data.title}</p>
+      }
+    )}
       <CreateArea additem={addItem} />
       {notes.map((note, index) => {
         return (
--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
 import express from "express";
+import cors from "cors";
+
+import fs from "fs";
 
 
 const app = express();
+
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+const file = fs.readFileSync("data.json");
+let data = JSON.parse(file);
+res.send(data);
+
+});
+
+app.post("/",(req,res)=>{
+    const newdata=req.body;
+  const file=  fs.readFileSync("data.json");
+  let data =[];
+  data=JSON.parse(file);
+  data.push(newdata);
+  fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
+
+res.json({result:"success",data:newdata});
+    
+})
 
 
 app.listen("5000",()=>{
